b615a49693ba4a44ea42d3b42f3b49dae0c7cc2e
aisolab <bsk0130@gmail.com>
2019-10-15 16:06:07 +0900

    To be modified

--- build_vocab.py
+++ build_vocab.py
@@ -1,15 +1,23 @@
 import pickle
 from model.utils import Vocab
-from pretrained.tokenization import BertTokenizer
+from transformers.tokenization_bert import BertTokenizer
 
 # loading BertTokenizer
-ptr_tokenizer = BertTokenizer.from_pretrained('pretrained/vocab.korean.rawtext.list', do_lower_case=False)
+ptr_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 idx_to_token = list(ptr_tokenizer.vocab.keys())
-
-# generate vocab
+token_to_idx = {token: idx for idx, token in enumerate(idx_to_token)}
 vocab = Vocab(idx_to_token, padding_token='[PAD]', unknown_token='[UNK]', bos_token=None, eos_token=None,
-              reserved_tokens=['[CLS]', '[SEP]', '[MASK]'], unknown_token_idx=1)
+              reserved_tokens=['[CLS]', '[SEP]', '[MASK]'], token_to_idx=token_to_idx)
 
 # save vocab
 with open('pretrained/vocab.pkl', mode='wb') as io:
     pickle.dump(vocab, io)
+
+
+from model.utils import Tokenizer, PadSequence, PreProcessor
+pad_sequence = PadSequence(length=10, pad_val=vocab.to_indices(vocab.padding_token))
+Tokenizer()
+preprocessor = PreProcessor(vocab=vocab, split_fn=ptr_tokenizer.tokenize,
+                            pad_fn=pad_sequence)
+preprocessor.preprocess('hi')
+
--- model/net.py
+++ model/net.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from pytorch_pretrained_bert.modeling import BertPreTrainedModel, BertModel
+from transformers.modeling_bert import BertPreTrainedModel, BertModel
 
 
 class BertClassifier(BertPreTrainedModel):
@@ -9,14 +9,12 @@ class BertClassifier(BertPreTrainedModel):
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, num_labels)
         self.vocab = vocab
-        self.apply(self.init_bert_weights)
+        self.init_weights()
 
-    def forward(self, input_ids, output_all_encoded_layers=False):
+    def forward(self, input_ids):
         # pooled_output is not same hidden vector corresponds to first token from last encoded layers
         attention_mask = input_ids.ne(self.vocab.to_indices(self.vocab.padding_token)).float()
-        if output_all_encoded_layers:
-            encoded_layers, pooled_output = self.bert(input_ids, None, attention_mask,
-                                                      output_all_encoded_layers=output_all_encoded_layers)
+        encoded_layers, pooled_output = self.bert(input_ids, attention_mask, None)
             encoded_layers = [encoded_layer[:, 0, :] for encoded_layer in encoded_layers]
             encoded_layers.append(pooled_output)
             pooled_output = self.dropout(pooled_output)
