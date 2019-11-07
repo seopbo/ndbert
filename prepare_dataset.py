9900f03d7a0a637b556f8ad1d9c43f62a34a8268
aisolab <bsk0130@gmail.com>
2019-11-08 00:55:06 +0900

    updated source code

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -8,8 +8,8 @@ from sklearn.model_selection import train_test_split
 parser = argparse.ArgumentParser()
 parser.add_argument('--ind', type=str, choices=["cr", "mpqa", "mr", "sst2", "subj", "trec"])
 parser.add_argument('--ood', type=str, choices=["cr", "mpqa", "mr", "sst2", "subj", "trec"])
-parser.add_argument('--dev_ind_size', type=int, default=500)
-parser.add_argument('--val_ind_size', type=int, default=2000)
+parser.add_argument('--dev_ind_size', type=int, default=1000)
+parser.add_argument('--val_ind_size', type=int, default=1000)
 
 
 if __name__ == '__main__':
@@ -41,10 +41,10 @@ if __name__ == '__main__':
         ood_all = pd.concat(ood_all, ignore_index=True, sort=False)
         ood_all = ood_all[~ood_all['document'].isna()]
 
-    tr_ind, val_ind = train_test_split(ind_all, test_size=args.val_ind_size, random_state=777)
+    tr_ind, test_ind = train_test_split(ind_all, test_size=args.val_ind_size, random_state=777)
     tr_ind, dev_ind = train_test_split(tr_ind, test_size=args.dev_ind_size, random_state=777)
 
-    tr_ood, val_ood = train_test_split(ood_all, test_size=args.val_ind_size, random_state=777)
+    tr_ood, test_ood = train_test_split(ood_all, test_size=args.val_ind_size, random_state=777)
     tr_ood, dev_ood = train_test_split(tr_ood, test_size=args.dev_ind_size, random_state=777)
 
     sub_dir = par_dir / '{}'.format(args.ood)
@@ -54,27 +54,27 @@ if __name__ == '__main__':
 
     tr_ind_path = str(par_dir / 'train_ind_{}.txt'.format(len(tr_ind)))
     dev_ind_path = str(par_dir / 'dev_ind_{}.txt'.format(len(dev_ind)))
-    val_ind_path = str(par_dir / 'test_ind_{}.txt'.format(len(val_ind)))
+    test_ind_path = str(par_dir / 'test_ind_{}.txt'.format(len(test_ind)))
     tr_ood_path = str(sub_dir / 'train_ood_{}.txt'.format(len(tr_ood)))
     dev_ood_path = str(sub_dir / 'dev_ood_{}.txt'.format(len(dev_ood)))
-    val_ood_path = str(sub_dir / 'test_ood_{}.txt'.format(len(val_ood)))
+    test_ood_path = str(sub_dir / 'test_ood_{}.txt'.format(len(test_ood)))
 
     tr_ind.to_csv(tr_ind_path, sep='\t', index=False)
     dev_ind.to_csv(dev_ind_path, sep='\t', index=False)
-    val_ind.to_csv(val_ind_path, sep='\t', index=False)
+    test_ind.to_csv(test_ind_path, sep='\t', index=False)
 
     tr_ood.to_csv(tr_ood_path, sep='\t', index=False)
     dev_ood.to_csv(dev_ood_path, sep='\t', index=False)
-    val_ood.to_csv(val_ood_path, sep='\t', index=False)
+    test_ood.to_csv(test_ood_path, sep='\t', index=False)
 
     par_config = Config({'train': tr_ind_path,
                          'dev': dev_ind_path,
-                         'test': val_ind_path})
+                         'test': test_ind_path})
     par_config.save(par_dir / 'config.json')
 
     sub_config = Config({'train': tr_ood_path,
                          'dev': dev_ood_path,
-                         'test': val_ood_path})
+                         'test': test_ood_path})
     sub_config.save(sub_dir / 'config.json')
 
     experiment_dir = Path('experiments') / '{}'.format(args.ind) / '{}'.format(args.ood)
